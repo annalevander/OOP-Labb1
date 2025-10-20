9902e2a0a86c672276173c4e3b7efb0051c0ce6d
annalevander <annalevander@icloud.com>
2025-10-20 18:33:33 +0200

    creatded a class triangle and a constructor for triangle and a method to get area of triangle

---
+++ OOP-Labb1/Triangle.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Labb1
+{
+    public class Triangle
+    {
+        double Base;
+        double Height;
+
+        public Triangle (int bas, int height)
+        {
+            Base = bas;
+            Height = height;
+        }
+        public double AreaOfTriangle()
+        {
+            double AreaTriangle = Base * Height / 2;
+            return AreaTriangle;
+        }
+    }
+}
